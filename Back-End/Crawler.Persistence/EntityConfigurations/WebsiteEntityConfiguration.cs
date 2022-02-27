8f9dd0b79f4e3276d9383286e16aee8cc75112b0
DESKTOP-D7K5720\DongTranDevOps <dongtrandevops@gmail.com>
2022-02-27 12:03:22 +0700

    Reference to submodule

--- Back-End/Crawler.Persistence/EntityConfigurations/WebsiteEntityConfiguration.cs
+++ Back-End/Crawler.Persistence/EntityConfigurations/WebsiteEntityConfiguration.cs
@@ -1,5 +1,5 @@
 ﻿using Crawler.Domain.Entities;
-using HelpDev.Common.Http;
+using CShapUtilities.Constants.Https;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
